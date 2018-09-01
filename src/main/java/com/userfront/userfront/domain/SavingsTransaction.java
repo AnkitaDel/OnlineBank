d4c45176f08db6d607c89053c0abf632896bf0a2
ankita <shikhasingh0410s@gmail.com>
2018-09-01 20:36:31 +0530

    delete

--- src/main/java/com/userfront/userfront/domain/SavingsTransaction.java
+++ src/main/java/com/userfront/userfront/domain/SavingsTransaction.java
@@ -1,4 +1,41 @@
 package com.userfront.userfront.domain;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class SavingsTransaction {
+
+    private long id;
+
+    private Date date;
+
+    private String description;
+
+    private String type;
+
+    public SavingsTransaction(Date date, String description, String type, String status, double amount, BigDecimal availableBalance, SavingsAccount savingsAccount) {
+        this.date = date;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.amount = amount;
+        this.availableBalance = availableBalance;
+        this.savingsAccount = savingsAccount;
+    }
+
+    private String status;
+
+    private double amount;
+
+    private BigDecimal availableBalance;
+
+    private SavingsAccount savingsAccount;
+
+    public SavingsTransaction()
+    {
+
+    }
+
+
+
 }
